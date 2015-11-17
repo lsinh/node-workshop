b3912b4485691804948777e970219a087b47b8b7
lsinh <leichin.sinha@gmail.com>
2015-11-17 23:33:15 +0000

    iss-augmented

--- iss.js
+++ iss.js
@@ -12,41 +12,44 @@ var prompt = require('prompt');
 
 
 //asking user for location
+console.log("Enter Your Location");
+
+prompt.start();
+
+prompt.get(['location'], function(err, result) {
+            var userLocation = result.location;
+
+            request('https://maps.googleapis.com/maps/api/geocode/json?address=' + userLocation, function(error, response, body) {
+                        if (!error && response.statusCode === 200) {
+                            var userLatLng = JSON.parse(body);
+                            var userLat = userLatLng.results[0].geometry.location.lat;
+                            var userLng = userLatLng.results[0].geometry.location.lng;
 
 
-request('http://api.open-notify.org/iss-now.json', function(error, response, body) {
-            if (!error && response.statusCode === 200) {
-                var issLatLng = JSON.parse(body);
-                var issLat = issLatLng.iss_position.latitude;
-                var issLng = issLatLng.iss_position.longitude;
-
-                prompt.get(['location'], function(err, result) {
-                            var userInput = result.location;
-
-                            request('https://maps.googleapis.com/maps/api/geocode/json?address=' + userInput, function(error, response, body) {
-                                if (!error && response.statusCode === 200) {
-                                    var userLatLng = JSON.parse(body);
-                                    var userLat = userLatLng.results[0].geometry.location.lat;
-                                    var userLng = userLatLng.results[0].geometry.location.lng;
+                            request('http://api.open-notify.org/iss-now.json', function(error, response, body) {
+                                        if (!error && response.statusCode === 200) {
+                                            var issLatLng = JSON.parse(body);
+                                            var issLat = issLatLng.iss_position.latitude;
+                                            var issLng = issLatLng.iss_position.longitude;
+                                        
 
 
-                                    var R = 6371000; // metres
-                                    var φ1 = userLat.toRadians();
-                                    var φ2 = issLat.toRadians();
-                                    var Δφ = (issLat - userLat).toRadians();
-                                    var Δλ = (issLng - userLng).toRadians();
+                                            var R = 6371000; // metres
+                                            var φ1 = userLat.toRadians();
+                                            var φ2 = issLat.toRadians();
+                                            var Δφ = (issLat - userLat).toRadians();
+                                            var Δλ = (issLng - userLng).toRadians();
 
-                                    var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-                                        Math.cos(φ1) * Math.cos(φ2) *
-                                        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-                                    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                                            var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+                                                Math.cos(φ1) * Math.cos(φ2) *
+                                                Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+                                            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-                                    var d = R * c;
+                                            var d = R * c;
 
-                                }
+
+                                            console.log("the distance between you and the ISS is " + d);
+                                       }); 
                                 
-                                console.log("the distance is " + d);
-                                
-                            }
-                            }
-                            );
+                            });
+                            });
