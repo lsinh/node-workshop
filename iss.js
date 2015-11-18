0a8d14798bb7a260e5952896e2aaccf20e272969
lsinh <leichin.sinha@gmail.com>
2015-11-18 17:29:37 +0000

    Iss-augmented, learned how to properly use callbacks and fixed syntax

--- iss.js
+++ iss.js
@@ -1,40 +1,30 @@
-//Code needed for program to run
-Number.prototype.toRadians = function() {
-    return this * Math.PI / 180;
-}
-
-
-
-//loading request modules
 var request = require('request');
 var prompt = require('prompt');
 
-
-
-//asking user for location
-console.log("Enter Your Location");
-
-prompt.start();
+//Code needed for program to run
+Number.prototype.toRadians = function() {
+    return this * Math.PI / 180; }
 
 prompt.get(['location'], function(err, result) {
             var userLocation = result.location;
+request('https://maps.googleapis.com/maps/api/geocode/json?address=' + userLocation, function (error, response, body) {
+    
+  if (!error && response.statusCode === 200) {
+    var theResult = JSON.parse(body);
+    
+    var userLatLng = theResult.results[0].geometry.location;
+    var userLat = userLatLng.lat;
+    var userLng = userLatLng.lng;
 
-            request('https://maps.googleapis.com/maps/api/geocode/json?address=' + userLocation, function(error, response, body) {
-                        if (!error && response.statusCode === 200) {
-                            var userLatLng = JSON.parse(body);
-                            var userLat = userLatLng.results[0].geometry.location.lat;
-                            var userLng = userLatLng.results[0].geometry.location.lng;
-
-
-                            request('http://api.open-notify.org/iss-now.json', function(error, response, body) {
+        console.log("Your coordinates: " + "lat: " + userLat.toFixed(2) + " lon: " + userLng.toFixed(2));
+   request('http://api.open-notify.org/iss-now.json', function(error, response, body) {
                                         if (!error && response.statusCode === 200) {
                                             var issLatLng = JSON.parse(body);
                                             var issLat = issLatLng.iss_position.latitude;
                                             var issLng = issLatLng.iss_position.longitude;
-                                        
-
-
-                                            var R = 6371000; // metres
+                                      
+                                            
+                                           var R = 6371000; // metres
                                             var φ1 = userLat.toRadians();
                                             var φ2 = issLat.toRadians();
                                             var Δφ = (issLat - userLat).toRadians();
@@ -46,10 +36,15 @@ prompt.get(['location'], function(err, result) {
                                             var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
                                             var d = R * c;
+                                            console.log("the distance between your location and the ISS is " + (d/1000).toFixed(2) +" kilometers");
+     }});
+  }});
+
+});
 
 
-                                            console.log("the distance between you and the ISS is " + d);
-                                       }); 
-                                
-                            });
-                            });
+                                            
+                                           
+                                        
+
+   
