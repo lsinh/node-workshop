db215623bfca59f0d7c51abf1657a5fd597a6e63
lsinh <leichin.sinha@gmail.com>
2015-11-18 04:36:39 +0000

    minor changes

--- guess.js
+++ guess.js
@@ -4,7 +4,7 @@ var rightNumber = Math.floor(Math.random() * 4) + 1;
 var turns = 4;
 
 function guessItRight() {
-  if (turns >= 0) {                                  
+  if (turns > 0) {                                  
     prompt.get(['userInput'], function (err, result)  {
             if (result.userInput == rightNumber){             //if we don't use a truthy operator, even returning the exact number will not yield a TRUE outcome
         console.log("You chose " + rightNumber + "." +"You win!");
